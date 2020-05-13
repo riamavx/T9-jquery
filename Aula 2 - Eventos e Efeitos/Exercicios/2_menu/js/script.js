d9411796f12d4e5b858172cba299824c8d433a4e
Maria Souza <messenger.mariah2017@gmail.com>
2020-05-13 09:13:57 -0300

    resolução de exercicios

---
+++ Aula 2 - Eventos e Efeitos/Exercicios/2_menu/js/script.js
@@ -0,0 +1,33 @@
+//Fazer o menu lateral aparecer
+
+//Primeira resolução
+
+// $(document).ready(function () {
+
+//     $('.botao-abrir').click(function () {
+//         $('#menulateral').show('slow')
+//     })
+
+//     $('.menu-lateral').click(function () {
+//         $('.menu-lateral').toggle('')
+//     })
+
+//lembrar de colocar marin-left
+
+
+// })
+
+//Segunda resolução
+
+// $(document).ready(function(){
+//     $("#botaoAbrir").click(function() {
+//         $("#menulateral").css("display", "block");
+//         $(".div-botao").css("margin-left", "250px")
+//     })
+//     $("#botaoFechar").click(function() {
+//         $("#menulateral").css("display", "none");
+//         $(".div-botao").css("margin-left", "0px")
+//     })
+// })
+
+// //dá pra resolver com hide()/show(), fadein(), fadeOut(), toggle()
